26e2a005e9dca598027d6b73588538a23e229dbc
ReaprendeDev <144256046+ReaprendeDev@users.noreply.github.com>
2026-03-20 17:50:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -131,7 +131,7 @@ function generatePdf(data, callback) {
             text: formattedDate, // Agregar la fecha
             fontSize: 16,
             alignment: 'left',
-            absolutePosition: { x: 495, y: 392 }, // Posición debajo del nombre
+            absolutePosition: { x: 493, y: 392 }, // Posición debajo del nombre
           },
         ],
       };
