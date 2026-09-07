d486b756b87e05f355717f6e0e985cd4908131d4
ReaprendeDev <144256046+ReaprendeDev@users.noreply.github.com>
2026-09-07 18:28:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -365,7 +365,7 @@ function generatePdf(data, callback) {
           },
           {
             text: `${data.nombre}`,
-            fontSize: 36,
+            fontSize: 38,
             alignment: 'center',
             absolutePosition: { x: 0, y: 230 },
           },
@@ -374,7 +374,7 @@ function generatePdf(data, callback) {
             fontSize: 16,
             alignment: 'left',
             color: '#000000', // Color de la letra
-            absolutePosition: { x: 457, y: 463 }, // Posición debajo del nombre
+            absolutePosition: { x: 457, y: 462 }, // Posición debajo del nombre
           },
         ],
       };  
